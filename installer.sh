471f61c3b3bcc60751a299463f48a8cbdf3cb922
Samuel Desmond Kurtzer <Email@SamuelKurtzer.com>
2021-04-12 16:03:48 +0930

    updating zshrc for fzf completion

--- installer.sh
+++ installer.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-
 if [[ -L ~/.Xmodmap ]]; then
+    echo "Installing Xmodmap File"
     ln -s ~/Dotfiles/Xmodmap ~/.Xmodmap
 fi
 if [[ -L ~/.vim/vimrc ]]; then
+    echo "installing vimrc files"
     ln -s ~/Dotfiles/vimrc ~/.vim/vimrc
 fi
 if [[ -L ~/.zshrc ]]; then
+    echo "installing zshrc file"
     ln -s ~/Dotfiles/zshrc ~/.zshrc
 fi
-if [[ -L ~/.gitconfig ]]; then
+if [[ -a ~/.gitconfig ]]; then
+    echo "installing Git config file"
     ln -s ~/Dotfiles/gitconfig ~/.gitconfig
 fi
-git config --global init.templatedir ~/Dotfiles/git
